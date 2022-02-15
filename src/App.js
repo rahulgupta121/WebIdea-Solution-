b506683a84306500725a06e19f80023a5369d7a4
rahulgupta121 <rahulkumar3.1996@gmail.com>
2022-02-15 17:12:25 +0530

    commit for update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className=" first-col "></div>
+      <div className="second-row">
+        <div className="second-row-main-div">
+          <h3 className="heading-company-1-1">Who Are We</h3>
+          <h4 className="heading-company-1-2">
+            We Are Leading IT Support Company
+          </h4>
+          <p>
+            Web Idea Solution LLP is not only an IT support team but we are the
+            backbone and the wall with possibilities that ensures you and your
+            business not only go but run dynamically on an smoother path which
+            is provided by our team of WIS, helping you always going extra miles
+            and with the best of solution to suit your individual needs.
+          </p>
+          <p>
+            We are the leading and reputed IT support companies in Sydney,
+            Australia which we have earned over time which the unimaginable
+            support of our team and their dedication towards bringing out the
+            best method that surpass any hurdle in our clients long run. We
+          </p>
+          <p>
+            just do not work, we bring changes to the flow of your business in
+            an lightning speed so that you grow high. Our happiness lies on our
+            client satisfactory.
+          </p>
+        </div>
+      </div>
+      <div className="container d-flex mt-5">
+        <div className=" first-part-col2 "></div>
+        <div className="second-part-row2">
+          <div className="second-part-row2-main-div">
+            <h3 className="heading-company-2-1"> Our Company </h3>
+            <h4 className="heading-company-2-2">
+              We Are India Software Development Company
+            </h4>
+            <p>
+              We are more then 10 year experience company, based located in
+              India managing over individual, small and large type IT firm with
+              dynamic support and services with successes. Web Idea Solution LLP
+              work hard and take our fun seriously. As an IT Support team
+              member, not only will you contribute to state of the art and
+              innovative projects and build a professional career, you will also
+              enjoy the benefits that turn your job into a passion. We move into
+              detail with you as your business is best known by you so as to
+              provide you with the best of us.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
